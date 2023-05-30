f908c5fe94d1c54d2f4431905c26bebf55ffd630
Bajron Ismailaj <caiupi@gmail.com>
2023-05-30 23:27:36 +0200

    sf

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
 import ExpenseItem from "./components/ExpenseItem";
-import ExerciseComponent from "./components/ExerciseComponent ";
-import Product from "./components/Product";
 
 function App() {
   const expenses = [
@@ -16,23 +14,6 @@ function App() {
     },
     {
       title: "Car Insurance3",
-      amount: 323.23,
-      date: new Date(2021, 2, 28),
-    },
-  ];
-  const products = [
-    {
-      title: "Product 1",
-      amount: 123.23,
-      date: new Date(2021, 2, 28),
-    },
-    {
-      title: "Product 2",
-      amount: 223.23,
-      date: new Date(2021, 2, 28),
-    },
-    {
-      title: "Product 3",
       amount: 323.23,
       date: new Date(2021, 2, 28),
     },
@@ -55,23 +36,6 @@ function App() {
         amount={expenses[2].amount}
         date={expenses[2].date}
       ></ExpenseItem>
-
-      <p>My Demo Shop</p>
-      <Product
-        title={products[0].title}
-        amount={products[0].amount}
-        date={products[0].date}
-      ></Product>
-      <Product
-        title={products[1].title}
-        amount={products[1].amount}
-        date={products[1].date}
-      ></Product>
-      <Product
-        title={products[2].title}
-        amount={products[2].amount}
-        date={products[2].date}
-      ></Product>
     </div>
   );
 }
--- src/components/ExerciseComponent .js
+++
@@ -1,7 +0,0 @@
-function ExerciseComponent(){
-    return (
-        <p1>First exercise - done!</p1>
-    );
-}
-
-export default ExerciseComponent;
