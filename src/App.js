60ef1a445eabb964dfeecd23aa3a92356acd03e3
Bajron Ismailaj <caiupi@gmail.com>
2023-06-26 00:47:03 +0200

    sd

--- src/App.js
+++ src/App.js
@@ -7,20 +7,20 @@ const App = () => {
   const expenses = [
     {
       id: 'e1',
-      title: 'Toilet Paper',
+      title: 'Toilet Paper 2',
       amount: 94.12,
       date: new Date(2020, 7, 14),
     },
     { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12) },
     {
       id: 'e3',
-      title: 'Car Insurance',
+      title: 'Car Insurance 2',
       amount: 294.67,
       date: new Date(2021, 2, 28),
     },
     {
       id: 'e4',
-      title: 'New Desk (Wooden)',
+      title: 'New Desk (Wooden) 2',
       amount: 450,
       date: new Date(2021, 5, 12),
     },
